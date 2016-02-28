dd69d122f0aed695591e7d008b7da16c9c26b13e
Sergio Perez <sperez4mba@gmail.com>
2016-02-28 15:29:18 -0600

    Added main route with index template

--- astoolsite/urls.py
+++ astoolsite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 from astool.urls import router
 
@@ -8,5 +9,6 @@ urlpatterns = [
     # url(r'^$', 'astoolsite.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/token/', obtain_auth_token, name='api-token'),
-    url(r'^api/', include(router.urls))
+    url(r'^api/', include(router.urls)),
+    url(r'^$', TemplateView.as_view(template_name="astool/index.html")),
 ]
